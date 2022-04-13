64f519c4f2c07ce979d870c890aa34c89d7e9138
cestangue001 <cestangue001@iutbayonne.univ-pau.fr>
2022-04-13 15:29:49 +0200

    Displaying customers and boilers

--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="container">
     <h1>Clients et chaudières</h1>
+    <h2>Clients</h2>
     <ul>
-      <li v-for="customer in customers" :key="customer.id">{{ customer.name }}</li>
+      <li v-for="customer in customers.data" :key="customer.id">
+        {{ customer.name }} {{ customer.firstName }}
+      </li>
+    </ul>
+    <h2>Chaudières</h2>
+    <ul>
+      <li v-for="boiler in boilers.data" :key="boiler.id">
+        {{ boiler.model }}
+      </li>
     </ul>
   </div>
 </template>
@@ -15,18 +24,31 @@ export default {
     return {
       loading: false,
       customers: [],
+      boilers: []
     };
   },
   mounted() {
-    this.refreshDoodles();
+    this.refreshCustomers();
+    this.refreshBoilers();
   },
   methods: {
-    refreshDoodles() {
+    refreshCustomers() {
       this.loading = true;
       api
-        .get("customer")
+        .get("customers")
         .then((response) => {
           this.customers = response.data;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    refreshBoilers() {
+      this.loading = true;
+      api
+        .get("boilers")
+        .then((response) => {
+          this.boilers = response.data;
         })
         .finally(() => {
           this.loading = false;
