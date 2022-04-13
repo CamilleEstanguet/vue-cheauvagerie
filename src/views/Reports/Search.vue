95f2e5d70705a2efb369f60815a130a88ca9e604
cestangue001 <cestangue001@iutbayonne.univ-pau.fr>
2022-04-13 16:53:50 +0200

    Display reports

--- src/views/Reports/Search.vue
+++ src/views/Reports/Search.vue
@@ -1,5 +1,47 @@
 <template>
-  <div>
-    <h1>Rapports</h1>
+  <div class="container">
+    <h1>Salons</h1>
+    <div class="display">
+      <h2>Clients</h2>
+      <div class="card" v-for="callout in callouts.data" :key="callout.id">
+        <div class="card-block">
+          <h4 class="card-title">
+            {{ callout.title }}
+          </h4>
+          <p class="card-text p-y-1">
+            Réalisée par le technicien {{ callout.techId }}
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import api from "../../api.js";
+
+export default {
+  data() {
+    return {
+      loading: false,
+      callouts: [],
+    };
+  },
+  mounted() {
+    this.refreshCallOuts();
+  },
+  methods: {
+    refreshCallOuts() {
+      this.loading = true;
+      api
+        .get("callouts")
+        .then((response) => {
+          this.callouts = response.data;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+  },
+};
+</script>
