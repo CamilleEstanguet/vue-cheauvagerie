a572fd965390968c6a3d21cc897105a5a195309c
Camille Estanguet <80349684+CamilleEstanguet@users.noreply.github.com>
2022-04-17 18:30:11 +0200

    Update api.js
    Address of the API changed

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://lakartxela.iutbayonne.univ-pau.fr/~cestangue001/api-laravelociclette/public/api/',
+  baseURL: 'http://localhost:8080/api/',
 })
 
 export default api
